8e1a089ae2928655c5c741fe6ca3e03272f0d72f
onichandame <zxinmyth@gmail.com>
2020-04-13 07:58:26 +0000

    added caget

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import express from 'express'
-import {} from 'epics'
+import { Channel } from 'epics'
 
-const port = isNaN(parseInt(process.env['PORT'])) ? 3000 : parseInt(process.env['PORT'])
+const port = isNaN(parseInt(process.env.PORT)) ? 3000 : parseInt(process.env.PORT)
 
 const app = express()
 
-app.get('*',(req,res)=>{
+app.get('*', (req, res) => {
+  const pvname = req.path.slice(1)
+  const pv = new Channel(pvname)
+    .connect((err:Error) => {
+      if (err) {
+        console.log(err)
+        res.status(500).send(JSON.stringify(err))
+      }
+      pv.get((error, data) => {
+        if (error) {
+          console.log(error)
+          res.status(500).send(JSON.stringify(error))
+        }
+        res.status(200).send(JSON.stringify(data))
+      })
+    })
 })
 
-app.listen(port, ()=>console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`))
--- src/types/epics.d.ts
+++ src/types/epics.d.ts
@@ -1,4 +1,34 @@
 declare module 'epics'{
-  export interface Channel{
+  export type State={
+    ECA_NORMAL : 1;
+    ECA_TIMEOUT : 80,
+    ECA_IODONE : 339,
+    ECA_ISATTACHED : 424,
+    OP_CONN_UP : 6,
+    OP_CONN_DOWN : 7,
+    CS_NEVER_CONN : 0,
+    CS_PREV_CONN: 1,
+    CS_CONN: 2,
+    CS_CLOSED: 3,
+    CS_NEVER_SEARCH : 4
+  }
+
+  type Option={
+    timeout?: number
+    fieldType?: string
+  }
+  type Data = number & string
+  export class Channel {
+    constructor (pvName:string)
+    public state(): State
+    public connected(): boolean
+    public connect(options?: Option, callback?:(err?:Error) => any): Channel
+    public connect(callback?:(err?:Error) => any): Channel
+    public disconnect(callback?:(err?:Error)=>any):void
+    public get(options?:Option, callback?:(err?:Error, data?: Data)=>any):Channel
+    public get(callback?:(err?:Error, data?: Data)=>any):Channel
+    public monitor(options?:Option):Channel
+    public put(value?:Data, callback?:(err?:Error)=>any):void
+    public put(callback?:(err?:Error)=>any):void
   }
 }
